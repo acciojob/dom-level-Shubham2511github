04a231e8d21efd28576aeb301e1fa927f625273b
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-05-22 23:27:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+window.onload = function () {
+const itemLvL = document.getElementById("level");
+let lvlcount = 0;
+while(itemLvL !== document){
+	itemLvL = itemLvL.parentNode;
+	lvlcount++;
+}
+alert("The level of the element is: "+ lvlcount)
+};
